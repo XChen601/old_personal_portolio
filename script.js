b5f0f78913acb69c64c2783508bdfa1c17edf70b
Xing Chen <72898664+XChen601@users.noreply.github.com>
2023-01-07 16:18:17 -0500

    Added cool hover effect with javascript, credit to Hyperplexed

---
+++ script.js
@@ -0,0 +1,15 @@
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+
+for(const item of document.querySelectorAll(".item")) {
+    item.onmousemove = e => handleOnMouseMove(e);
+}
